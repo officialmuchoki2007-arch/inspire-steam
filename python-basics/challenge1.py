8ee5c9f7374b252de95e29e644c45883e43770bd
Muchoki <official.muchoki2007@gmail.com>
2026-02-11 09:16:53 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university = "JKUAT"
+grade = "A"
+favourite_course = "engineering"
+cluster_points = 80
+
+print(f"I am a student at {university}")
+print(f"I got a grade of {grade} in my exams")
+print(f"My favourite course is {favourite_course}")
+print(f"I scored {cluster_points} points in my cluster exams")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This a comment
+# my hobbies
+print("Hello from Muchoki")
+print("I am from thika")
+# wagwan
+print("I like reading novels,watching movies and sleeping")
+
+age = 18
+height = 1.75
+weight = 60
+
+print(f" I am {age} years old")
+print(f" I am also {height} metres tall")
+print(f" And wiegh {weight} kgs")
