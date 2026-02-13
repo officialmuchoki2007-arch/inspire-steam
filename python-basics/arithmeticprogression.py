77a328192bea848541de48dcc51064ad78d5a5b9
Muchoki <official.muchoki2007@gmail.com>
2026-02-13 12:09:36 +0300

    Add files via upload

---
+++ python-basics/arithmeticprogression.py
@@ -0,0 +1,16 @@
+# Name : Stacy Muchoki
+# Date : 13/02/2026operations
+# Program to calculate arithmetic progression
+
+
+# calculating nth term
+
+a = int(input("enter the first number :"))
+n = int(input("enter the number of terms :"))
+d = int(input("enter the common difference"))
+
+nth_term = a + (n - 1) * d
+print(f"the nth_term is: {nth_term}") 
+
+sn = (n / 2) * (2 * a +(n - 1) * d)
+print(f"the sum of terms is: {sn}")
